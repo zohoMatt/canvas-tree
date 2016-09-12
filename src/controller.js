eaaef80bbe5ce1b4be5b40a8f4d5988e4471817a
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-09-12 10:24:01 +0800

    feat: Add controller

--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,19 @@
 /**
  * Created by Zoho on 16/9/12.
  */
+let ControllerData = function (name, speed, quantity) {
+    this.name = name;
+    this.speed = speed;
+    this.quantity = quantity;
+};
+
+window.onload = function () {
+    // Initialize interface
+    let panel = new dat.GUI();
+    let data = new ControllerData('Control Panel', 200, 30);
+
+    // Set Controllers
+    // panel.add(data, 'name');
+    let speedControl = panel.add(data, 'speed', 0, 800);
+    let quantityControl = panel.add(data, 'quantity', 0, 100);
+};
