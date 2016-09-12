a557079ca305ebcb7bc84c4feec12aec8e35e240
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-09-12 16:01:23 +0800

    feat: class Circle, class EnhancedContext. feat: Connect data with controller. feat: Trail movement.

--- src/controller.js
+++ src/controller.js
@@ -1,19 +1,31 @@
 /**
  * Created by Zoho on 16/9/12.
  */
-let ControllerData = function (name, speed, quantity) {
+let DataStore = function (name, speedX, speedY, quantity) {
     this.name = name;
-    this.speed = speed;
+    this.speedX = speedX;
+    this.speedY = speedY;
     this.quantity = quantity;
 };
 
-window.onload = function () {
-    // Initialize interface
-    let panel = new dat.GUI();
-    let data = new ControllerData('Control Panel', 200, 30);
+/******************** Set panel *******************/
+let data = new DataStore('Controller', 0.2, 0.1, 30);
+let panel = new dat.GUI();
+let speedXControl = panel.add(data, 'speedX', -2, 2);
+let speedYControl = panel.add(data, 'speedY', -2, 2);
+let quantityControl = panel.add(data, 'quantity', 0, 100);
 
-    // Set Controllers
-    // panel.add(data, 'name');
-    let speedControl = panel.add(data, 'speed', 0, 800);
-    let quantityControl = panel.add(data, 'quantity', 0, 100);
-};
+/******************** Add event listeners *******************/
+speedXControl.onChange((value) => {
+    data.speedX = value;
+});
+
+speedYControl.onChange((value) => {
+    data.speedY = value;
+});
+
+quantityControl.onChange((value) => {
+    data.quantity = value;
+});
+
+export default data;
--- src/trail.js
+++ src/trail.js
@@ -1,3 +1,48 @@
 /**
  * Created by Zoho on 16/9/12.
  */
+import { Circle, EnhancedContext } from './CanvasLibrary';
+import Data from './controller';
+
+const canvas = document.getElementById('main');
+
+let context;
+// test browser support
+if (canvas.getContext) {
+    context = canvas.getContext('2d');
+} else {
+    alert('Sorry that you can not watch this demo because your browser does not support canvas.')
+}
+
+let ctxPlus = new EnhancedContext(canvas, context);
+
+const a = new Circle('circle', {
+    x: 300,
+    y: 300,
+    radius: 40,
+    fillColor: 'blue',
+    radian: Math.PI * 2
+}, {
+    speed_x: 1,
+    speed_y: 1
+});
+
+
+ctxPlus.placeItem(a);
+
+/******************** Movement *******************/
+const run = () => {
+    const { speedX, speedY } = Data;
+    const opacity = 0.1;
+    a.adjustAcceleration(speedX, speedY);
+
+    const { x, y } = a.getPosition();
+    if (a.isOut('entire', canvas)) {
+        a.jumpTo(0, 0);
+    }
+
+    ctxPlus.moveItemOnce(a, opacity);
+    window.requestAnimationFrame(run);
+};
+
+window.requestAnimationFrame(run);
